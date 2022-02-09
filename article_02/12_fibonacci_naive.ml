2516f6e6361c96b1590132943e033954ddf17f7e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-09 09:40:54 +0100

    Example 12: Fibonacci sequence, naive implementation

---
+++ article_02/12_fibonacci_naive.ml
@@ -0,0 +1,11 @@
+(* Naivní implementace výpočtu Fibonacciho posloupnosti *)
+
+fun fib n =
+    if n = 0 then 0 else
+    if n = 1 then 1 else
+    fib (n - 1) + fib (n - 2);
+
+
+fib 0;
+fib 1;
+fib 10;
