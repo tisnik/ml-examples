8f64dad330897f658cf439b33eaeb4c681bb86e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-09 09:40:54 +0100

    Example 13: Fibonacci sequence based on pattern matching

---
+++ article_02/13_fibonacci_pattern_matching.ml
@@ -0,0 +1,10 @@
+(* Implementace výpočtu Fibonacciho posloupnosti s využitím pattern matchingu *)
+
+fun fib 0 = 0
+  | fib 1 = 1
+  | fib n = fib (n - 1) + fib (n - 2);
+
+
+fib 0;
+fib 1;
+fib 10;
