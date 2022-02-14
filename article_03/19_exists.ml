7e6220a70a3f139391bb268897dd8df6e813c9a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-14 08:55:48 +0100

    Example #19: exists function

---
+++ article_03/19_exists.ml
@@ -0,0 +1,14 @@
+(* Operátor exists *)
+
+fun exists(element, []) = false
+  | exists(element, head::tail) = if element = head then true
+                                  else exists(element, tail);
+
+
+infix 1 exists;
+
+
+1+1 exists [1,2,3];
+3*1 exists [1,2,3];
+
+1+1 exists [1,2] @ [3,4];
