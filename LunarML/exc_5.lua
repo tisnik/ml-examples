11ee43872c5f60da0a2565382bc5e8b0653d52f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-11 11:51:40 +0200

    Exception handling, transpiled into Lua

---
+++ LunarML/exc_5.lua
@@ -0,0 +1,153 @@
+local error = error
+local getmetatable = getmetatable
+local pcall = pcall
+local setmetatable = setmetatable
+local math = math
+local math_mininteger = math.mininteger
+local string = string
+local string_format = string.format
+local table = table
+local table_concat = table.concat
+local _exn_meta = {}
+function _exn_meta:__tostring()
+  local traceback = self.traceback
+  if traceback then
+    traceback = "\n" .. traceback
+  else
+    traceback = ""
+  end
+  return string_format("%s: %s%s", self.location or "<no location info>", self.tag[1], traceback)
+end
+local _Match_tag = { "Match" }
+local _Match = setmetatable({ tag = _Match_tag }, _exn_meta)
+local _Overflow_tag = { "Overflow" }
+local _Overflow = setmetatable({ tag = _Overflow_tag }, _exn_meta)
+local _Div_tag = { "Div" }
+local _Div = setmetatable({ tag = _Div_tag }, _exn_meta)
+local _Fail_tag = { "Fail" }
+local function __exn_instanceof(e, tag)
+  return getmetatable(e) == _exn_meta and e.tag == tag
+end
+local function _raise(x, location)
+  local e
+  if getmetatable(x) == _exn_meta and location ~= nil then
+    local traceback = debug.traceback(nil, 2)
+    e = setmetatable({ tag = x.tag, payload = x.payload, location = location, traceback = traceback }, _exn_meta)
+  else
+    e = x
+  end
+  error(e, 1)
+end
+local function _Int_div(x, y)
+  -- assert(math_type(x) == "integer")
+  -- assert(math_type(y) == "integer")
+  if y == 0 then
+    _raise(_Div, "Int.div")
+  elseif x == math_mininteger and y == -1 then
+    _raise(_Overflow, "Int.div")
+  end
+  return x // y
+end
+local function _VectorOrArray_fromList(xs)
+  local t = {}
+  local n = 0
+  while xs ~= nil do
+    n = n + 1
+    t[n] = xs[1]
+    xs = xs[2]
+  end
+  t.n = n
+  return t
+end
+local tostring, gsub, outputAndFlush, stdOut, y
+do
+  local io = _ENV.io
+  tostring = _ENV.tostring
+  local stdout = io.stdout
+  gsub = string.gsub
+  local Io_tag = {"Io"}
+  local BlockingNotSupported = setmetatable({tag = {"BlockingNotSupported"}}, _exn_meta)
+  local LINE_BUF = "LINE_BUF"
+  outputAndFlush = function(_1, content)
+    if _1.tag == "LUA_WRITABLE" then
+      local writable = _1.payload.writable
+      local name = _1.payload.name
+      local tmp, tmp1 = writable:write(content)
+      if not tmp then
+        _raise(setmetatable({tag = Io_tag, payload = {cause = setmetatable({tag = _Fail_tag, payload = tmp1}, _exn_meta), ["function"] = "output", name = name}}, _exn_meta), "text-io.sml:290:43")
+      else
+        writable:flush()
+        return nil
+      end
+    elseif _1.tag == "PRIM_WRITER" then
+      local name = _1.payload.writer.name
+      local writeVec = _1.payload.writer.writeVec
+      local buffer = _1.payload.buffer
+      local tmp
+      do
+        local tmp1, tmp2 = {content, buffer[1]}, nil
+        ::cont::
+        local _1_1, ys = tmp1, tmp2
+        if _1_1 == nil then
+          tmp = ys
+        elseif _1_1 ~= nil then
+          local tmp3 = _1_1[1]
+          local tmp4 = _1_1[2]
+          tmp1 = tmp4
+          tmp2 = {tmp3, ys}
+          goto cont
+        else
+          _raise(_Match, "list.sml:60:5")
+        end
+      end
+      local tmp1 = _VectorOrArray_fromList(tmp)
+      local content_PRIME = table_concat(tmp1)
+      if writeVec.tag == "SOME" then
+        local writeVec1 = writeVec.payload
+        writeVec1({base = content_PRIME, length = #content_PRIME, start = 0})
+        buffer[1] = nil
+        return nil
+      elseif writeVec.tag == "NONE" then
+        _raise(setmetatable({tag = Io_tag, payload = {cause = BlockingNotSupported, ["function"] = "output", name = name}}, _exn_meta), "text-io.sml:397:26")
+      else
+        _raise(_Match, "text-io.sml:395:14")
+      end
+    else
+      _raise(_Match, "text-io.sml:392:9")
+    end
+  end
+  stdOut = {tag = "LUA_WRITABLE", payload = {buffer_mode = {LINE_BUF}, name = "<stdout>", writable = stdout}}
+  local DivideByZero_tag = {"DivideByZero"}
+  local divide = function(x)
+    return function(y1)
+      if y1 == 0 then
+        local tmp = gsub(tostring(x), "-", "~")
+        _raise(setmetatable({tag = DivideByZero_tag, payload = "divide " .. tmp .. " by zero"}, _exn_meta), "exc_5.sml:3:18")
+      else
+        return _Int_div(x, y1)
+      end
+    end
+  end
+  local tmp = divide(10)
+  local x = tmp(5)
+  local tmp1 = gsub(tostring(x), "-", "~")
+  outputAndFlush(stdOut, tmp1)
+  outputAndFlush(stdOut, "\n")
+  local status, exn = pcall(function()
+    local tmp2 = divide(10)
+    return tmp2(0)
+  end)
+  if not status then
+    if __exn_instanceof(exn, DivideByZero_tag) then
+    else
+      _raise(exn, nil)
+    end
+    outputAndFlush(stdOut, exn.payload)
+    y = 0
+  else
+    y = exn
+  end
+end
+local tmp = gsub(tostring(y), "-", "~")
+outputAndFlush(stdOut, tmp)
+outputAndFlush(stdOut, "\n")
